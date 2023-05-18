830e79430fb7c4641d12eef6f70863721cf2c284
Alexsandro souza <alexsandrosouzasilva68@gmail.com>
2023-05-18 17:33:44 -0300

    Condicionais

---
+++ script.js
@@ -0,0 +1,27 @@
+
+const carros = ('Gol' Palio', 'Corsa' Celta);
+//const newCarros = carros.push('oi');
+
+//console.log(newCarros)
+
+/*nomeCarros = ()=>{
+    if(resultado = undefined){
+        return console.log("Esse indice não existe")
+    }
+    resultado = console.log(carros[5]);
+    
+    
+}
+
+nomeCarros();*/
+
+//Condicionais
+
+/*
+>= Maior ou igual
+<= Menor ou igual
+= Igual
+!= Diferente
+&& E
+|| Ou
+*/
