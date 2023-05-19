a317b3e6765b3dc1f48f512df829537bb3265599
Alexsandro souza <alexsandrosouzasilva68@gmail.com>
2023-05-19 15:37:21 -0300

    Manipulando o objeto Data

--- script.js
+++ script.js
@@ -26,10 +26,10 @@ texto('I love')
 
 //while = Enquanto (verificar a condição primeiro para depois fazer a ação)
 //For = é bem parecido com while a única diferença é a forma de declaração
-//ForEach
+//ForEach = específico para fazer loop em array, objeto e json
 //do = Faz a ação antes de verificar a condição
 
-a = 0
+/*a = 0
 
     while(a < 5){
         console.log(a)
@@ -39,4 +39,16 @@ a = 0
 
 for(a = 0; a < 5; a++){ //Diferente do while o for resume todos os parametros numa linha
     console.log(a)
+}*/
+
+//Manupulando datas
+
+const data = new Date;
+let dia = data.getDay();
+
+if(dia == 4){
+    dia = "Sexta"
+    console.log(dia);
+}else{
+    console.log("Hoje não é sexta")
 }
